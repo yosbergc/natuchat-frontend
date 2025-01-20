c8def52fc726d71b9cbbedcfb3c67dbe6f788e4b
Yosber Guerrero <yosbergc@gmail.com>
2025-01-19 20:32:38 -0400

    Added Chat Messages component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import './App.css'
 import { useChat } from './hooks/useChat'
 import { SendMessage } from './components/SendMessage/SendMessage'
-
+import { ChatMessages } from './components/ChatMessages/ChatMessages'
 function App() {
   const { chat, setChat } = useChat()
   return (
     <>  
       <main>
         <section className="chat-container">
-          <section className="chat-messages">
-
-          </section>
+          <ChatMessages />
           <SendMessage />
         </section>
         
---
+++ src/components/ChatMessages/ChatMessages.tsx
@@ -0,0 +1,8 @@
+import './chatmessages.css'
+function ChatMessages() {
+    return (
+        <section className="chat-messages">
+        </section>
+    )
+}
+export { ChatMessages }
